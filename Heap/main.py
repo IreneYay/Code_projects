b67150059f0e847aeb291c6484c951e16d147c49
IreneYay <48572145+IreneYay@users.noreply.github.com>
2019-08-24 03:01:36 -0400

    Create main.py

---
+++ Heap/main.py
@@ -0,0 +1,12 @@
+from Heap import find_median
+
+
+def main(filename):
+    with open(filename, 'r') as reader:
+        text = [line.strip() for line in reader]
+        median = find_median(text)
+        print('Median:', median)
+
+
+if __name__ == '__main__':
+    main('alphabet.txt')
